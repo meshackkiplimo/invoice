366cdfa330d318267f53207573162e029e0bca8f
kimaiyo <meshackkimaiyo5@gmail.com>
2025-04-12 21:45:20 +0300

    fix: correct artificial delay in fetchRevenue and update LatestInvoices title

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -17,11 +17,11 @@ export async function fetchRevenue() {
     // Don't do this in production :)
 
     console.log('Fetching revenue data...');
-    await new Promise((resolve) => setTimeout(resolve, 30000));
+    await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<Revenue[]>`SELECT * FROM revenue`;
 
-    console.log('Data fetch completed after 30 seconds.');
+    console.log('Data fetch completed after 3seconds.');
 
     return data;
   } catch (error) {
@@ -37,7 +37,7 @@ export async function fetchLatestInvoices() {
       FROM invoices
       JOIN customers ON invoices.customer_id = customers.id
       ORDER BY invoices.date DESC
-      LIMIT 5`;
+      LIMIT 9`;
 
     const latestInvoices = data.map((invoice) => ({
       ...invoice,
